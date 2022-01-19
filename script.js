ade242a268f06489adc121e3f9552fafac1b8456
Thiago Vieira <jaumsix6@gmail.com>
2022-01-19 13:47:32 -0300

    att

--- script.js
+++ script.js
@@ -1,31 +1,7 @@
-const items = document.querySelectorAll('.accordion button')
+const faqs = document.querySelectorAll(".faq");
 
-function toggleAccordion() {
-    const itemToggle = this.getAttribute('aria-expanded')
-
-    for(i=0; i < items.length; i++) {
-        items[i].setAttribute('aria-expanded', 'false');
-    }
-
-    if(itemToggle == 'false') {
-        this.setAttribute('aria-expanded', 'true')
-    }
-}
-
-items.forEach((item) => item.addEventListener('click', toggleAccordion));
-
-$(document).ready(function(){
-    $("a").on('click', function(event) {
-  
-      if (this.hash !== "") {
-        event.preventDefault();
-        var hash = this.hash;
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 800, function(){
-     
-          window.location.hash = hash;
-        });
-      }
-    });
+faqs.forEach(faq => {
+  faq.addEventListener("click", () => {
+    faq.classList.toggle("active");
   });
+})
